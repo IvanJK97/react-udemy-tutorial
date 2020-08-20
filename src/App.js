fb5f70190e1118f0739e64c7683809558145b878
Ivan Kok <Ivan.Kok@bentley.com>
2020-08-20 17:39:48 -0400

    React Hooks tutorial - Lecture 44

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
+const App = props => {
+  // destructuring array - useState returns two things, the state and a function to modify state
+  const [ personsState, setPersonsState] = useState({
+    persons: [
+      {
+        name: 'Max',
+        age: 28
+      },
+      {
+        name: 'Manu',
+        age: 29
+      },
+      {
+        name: 'Stephanie',
+        age: 26
+      }
+    ]
+  })
+
+  // Separate state into different slices with multiple useState
+  const [otherState, setOtherState] = useState('some other value');
+
+  console.log(personsState, otherState);
+
+  const switchNameHandler = () => {
+    setPersonsState({
+      persons: [
+        {name: 'Maximilian', age: 28},
+        {name: 'Manu', age: 29},
+        {name: 'Stephanie', age: 27}
+      ]
+    });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi I'm a React App</h1>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My hobbies: racing</Person>
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
     </div>
   );
 }
 
+// State is a special property/variable of the class
+// this.setState automatically merges new state with old state, updating changed values - but not with useState
+// What JSX actually gets compile to - React.createElement(...)
 export default App;
