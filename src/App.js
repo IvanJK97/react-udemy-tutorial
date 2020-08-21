229084ca657d91004b2cd6ba81cb79dec0b3e8cc
Ivan Kok <Ivan.Kok@bentley.com>
2020-08-21 12:44:55 -0400

    Finished Section 3 - Base Syntax Course Content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const App = props => {
-  // destructuring array - useState returns two things, the state and a function to modify state
-  const [ personsState, setPersonsState] = useState({
+class App extends Component {
+  // State is a special property/variable of the class
+  state = {
     persons: [
       {
         name: 'Max',
@@ -19,34 +19,75 @@ const App = props => {
         age: 26
       }
     ]
-  })
-
-  // Separate state into different slices with multiple useState
-  const [otherState, setOtherState] = useState('some other value');
-
-  console.log(personsState, otherState);
-
-  const switchNameHandler = () => {
-    setPersonsState({
-      persons: [
-        {name: 'Maximilian', age: 28},
-        {name: 'Manu', age: 29},
-        {name: 'Stephanie', age: 27}
-      ]
-    });
   }
-  return (
-    <div className="App">
-      <h1>Hi I'm a React App</h1>
-      <button onClick={switchNameHandler}>Switch Name</button>
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My hobbies: racing</Person>
-      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
-    </div>
-  );
+
+  switchNameHandler = () => {
+    // console.log("Click");
+    this.setState({
+      persons: [
+        {
+          name: 'Maxim',
+          age: 28
+        },
+        {
+          name: 'Manu',
+          age: 29
+        },
+        {
+          name: 'Stephanie',
+          age: 27
+        }
+      ],
+      otherState: 'some other value'
+    })
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        {
+          name: 'Maxim',
+          age: 28
+        },
+        {
+          name: event.target.value,
+          age: 29
+        },
+        {
+          name: 'Stephanie',
+          age: 27
+        }
+      ]
+    })
+  }
+
+  render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    // Explanation of this keyword in assignment solution 11:53
+    // setState is provided by component while React is needed for JSX to convert to React.createElement
+    return (
+      <div className="App">
+        <h1>Hi I'm a React App</h1>
+        <button 
+          style={style}
+          onClick={this.switchNameHandler}>Switch Name
+        </button>
+        <div>
+          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+          <Person changed={this.nameChangedHandler} name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: racing</Person>
+          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        </div>
+      </div>
+    );
+  }
 }
 
-// State is a special property/variable of the class
-// this.setState automatically merges new state with old state, updating changed values - but not with useState
 // What JSX actually gets compile to - React.createElement(...)
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
   return (
-    <div>
-        {console.log("hi")}
+    <div className='Person'>
         <p>I'm {props.name} and I am {props.age} years old!</p>
         <p>{props.children}</p>
+        <input type="text" onChange={props.changed} value={props.name}/>
     </div>
   )
 }
