5c0f611ae9d39b6a9fcc60cdf36c7fe796b44485
Ivan Kok <Ivan.Kok@bentley.com>
2020-08-29 23:12:41 -0400

    Finished section 4: Working with lists and conditionals

--- src/App.js
+++ src/App.js
@@ -1,89 +1,82 @@
-import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
   // State is a special property/variable of the class
   state = {
     persons: [
-      {
-        name: 'Max',
-        age: 28
-      },
-      {
-        name: 'Manu',
-        age: 29
-      },
-      {
-        name: 'Stephanie',
-        age: 26
-      }
-    ]
-  }
+      { id: "1a", name: "Max", age: 28 },
+      { id: "2b", name: "Manu", age: 29 },
+      { id: "3c", name: "Stephanie", age: 26 },
+    ],
+    showPersons: false,
+  };
 
-  switchNameHandler = () => {
-    // console.log("Click");
-    this.setState({
-      persons: [
-        {
-          name: 'Maxim',
-          age: 28
-        },
-        {
-          name: 'Manu',
-          age: 29
-        },
-        {
-          name: 'Stephanie',
-          age: 27
-        }
-      ],
-      otherState: 'some other value'
-    })
-  }
+  nameChangeHandler = (event, id) => {
+    // Find the index in persons array that matches id
+    const personIndex = this.state.persons.findIndex(
+      (person) => person.id === id
+    );
+    const newPerson = {
+      ...this.state.persons[personIndex],
+      name: event.target.value,
+    };
+    const persons = [...this.state.persons];
+    persons[personIndex] = newPerson;
+    this.setState({ persons });
+  };
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        {
-          name: 'Maxim',
-          age: 28
-        },
-        {
-          name: event.target.value,
-          age: 29
-        },
-        {
-          name: 'Stephanie',
-          age: 27
-        }
-      ]
-    })
-  }
+  togglePersonsHandler = () => {
+    this.setState({ showPersons: !this.state.showPersons });
+  };
+
+  deletePersonHandler = (personId) => {
+    // Should always modify a copy of the state instead of modifying directly (modify immutably)
+    // const persons = this.state.persons.slice(personIndex) or const persons = [...this.state.persons];
+    // persons.splice(personIndex, 1); // Can "modify" a const because this is a reference to an array. We didn't change the reference but modified the array it points to
+    const persons = this.state.persons.filter(
+      (person) => person.id !== personId
+    ); // Will create a new array with filter func
+    this.setState({ persons });
+  };
 
   render() {
     const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer",
     };
+
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person) => (
+            <Person
+              key={person.id}
+              personId={person.id}
+              name={person.name}
+              age={person.age}
+              changed={this.nameChangeHandler}
+              delete={this.deletePersonHandler}
+            />
+          ))}
+        </div>
+      );
+    }
 
     // Explanation of this keyword in assignment solution 11:53
     // setState is provided by component while React is needed for JSX to convert to React.createElement
     return (
       <div className="App">
         <h1>Hi I'm a React App</h1>
-        <button 
-          style={style}
-          onClick={this.switchNameHandler}>Switch Name
+        <button style={style} onClick={this.togglePersonsHandler}>
+          Toggle Persons
         </button>
-        <div>
-          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-          <Person changed={this.nameChangedHandler} name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: racing</Person>
-          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-        </div>
+        {persons}
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import './Person.css';
+import React from "react";
+import "./Person.css";
 
 const person = (props) => {
   return (
-    <div className='Person'>
-        <p>I'm {props.name} and I am {props.age} years old!</p>
-        <p>{props.children}</p>
-        <input type="text" onChange={props.changed} value={props.name}/>
+    <div className="Person">
+      <p>
+        I'm {props.name} and I am {props.age} years old!
+      </p>
+      <p>{props.children}</p>
+      <input
+        type="text"
+        onChange={(event) => props.changed(event, props.personId)}
+        value={props.name}
+      />
+      <button onClick={() => props.delete(props.personId)}>Delete</button>
     </div>
-  )
-}
+  );
+};
 
 export default person;
